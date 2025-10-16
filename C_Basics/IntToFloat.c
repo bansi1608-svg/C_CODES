26014d24fc308d47f3218fe133ff97c3886aa3d0
Bansi Mehta <bansimehta@Bansis-MacBook-Air.local>
2025-10-16 10:54:31 +0530

    latest

---
+++ C_Basics/IntToFloat.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    float x;
+    x=9.7;
+    int y;
+    y=x;
+    float z = x - y ;
+    printf("FPF of given number is: %f",z);return 0;
+}
---
+++ C_Basics/SumofTwoNumbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    float x;
+    printf("Enter first number : \n ");
+    scanf("%f",&x) ;
+    float y;
+    printf("Enter second number : \n ");
+    scanf("%f",&y);
+    float sum = x + y ;
+    printf("The sum of 2 numbers is: %f",sum);
+    return 0;
+
+}
